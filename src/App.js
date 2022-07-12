874cc283f5e98040ed3dd90153251afe656497a8
agustinOttonello <agustinottonello@gmail.com>
2022-07-12 14:56:45 -0300

    Primera version

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Proyecto de ecommerce de agustin ottonello</h1>
+    </div>
+  );
+}
+
+export default App;

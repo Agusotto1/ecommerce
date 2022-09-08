04c6f6275a07586e402d9ecf568b2f6530d86c40
agustinOttonello <agustinottonello@gmail.com>
2022-09-08 20:44:55 -0300

    proyecto final v2

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,8 +2,15 @@ const Contact =() =>{
 
     return (
         <div>
-            <h1>acerca de nosotros</h1>
-
+            <h1>Acerca de nosotros</h1>
+            <h2>Somos una empresa dedicada a la venta de equipos celulares de todo tipo <br />
+                                
+            </h2>
+            <h3>
+            Tenemos desde los primeros moviles hasta la ultima tecnologia del mercado <br />
+                ¡Aqui seguramente conseguiras el telefono que estas buscando!
+                
+            </h3>
 
         </div>
     )
